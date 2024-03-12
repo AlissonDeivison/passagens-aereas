8ebf97cdfdc9c4f8e4c45e9c2e6cb4ee0f3e5116
Alisson Deivison S Pereira <uepbalisson@gmail.com>
2024-03-12 16:43:34 -0300

    Atualiza o componente de banner para exibir uma imagem

--- src/app/shared/banner/banner.component.ts
+++ src/app/shared/banner/banner.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './banner.component.scss'
 })
 export class BannerComponent {
-
+  @Input ()  src : String = ''
+  @Input ()  alt : String = ''
 }
