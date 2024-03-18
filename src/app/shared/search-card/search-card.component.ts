e31ba5ede19005ee58fc82aa32da0b6cadfb29dc
Alisson Deivison S Pereira <uepbalisson@gmail.com>
2024-03-18 18:40:49 -0300

    Adiciona componente de busca

---
+++ src/app/shared/search-card/search-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-card',
+  templateUrl: './search-card.component.html',
+  styleUrl: './search-card.component.scss'
+})
+export class SearchCardComponent {
+
+}
